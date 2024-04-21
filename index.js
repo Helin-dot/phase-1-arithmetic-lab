56e32b878bf5b916f58a6bd354e2c928bf7b0dc0
helin <helin.tayeb1@gmail.com>
2024-04-21 15:32:31 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1= 2
+let num2= 31
+let num3= 19
+let num4= 20
+let multiply = num1 * num2
+console.log(multiply)
+
+
+let random= Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let  mod = 25 % 7;
+
+
+let max= Math.max(num1, num3, num4)
+console.log(max)
